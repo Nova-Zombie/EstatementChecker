d398f3191e0497025353639fd6b512f681c70025
Nova-Zombie <64931815+Nova-Zombie@users.noreply.github.com>
2020-07-20 18:30:50 -0400

    Added a search funtion to allow the search of keywords and add loop to alow for multiple searches

--- errorCatcherForStatements.py
+++ errorCatcherForStatements.py
@@ -1,7 +1,8 @@
 import re
 import datetime
 
-def errorCatcherForStatements():
+def errorCatcherForStatements(checkKeyWord):
+
     statments = open("Estatements/checker.txt")
     statments1 = statments.readlines()
     total = 0
@@ -9,8 +10,7 @@ def errorCatcherForStatements():
     timeStart = datetime.datetime.now()
 
     for x in statments1:
-        if "FRAME" in x: #searches for FRAME in statements
-
+        if str(checkKeyWord).lower() in x.lower(): #searches for user key word and converts it to a string
             val = re.search("[$]\d*[.]\d{2}", x)
             cutMoneySign = val.group().split('$') #returns an array of re.search - $ sign
             value = float(cutMoneySign[1]) #converts the reg exp to an int
@@ -24,13 +24,13 @@ def errorCatcherForStatements():
     timeDif = (float(timeStop.strftime("%f")) - float(timeStart.strftime("%f"))) *.000001
     timeDifstr = "{:.6f}"
     print()
-    print(timeDifstr.format(timeDif) , "milliseconds to complete")
+    print(timeDifstr.format(timeDif) , "milliseconds to complete \n")
     #print(timeStart)
-    #print(timeStop)
+    #print(timeStop)asdfasdf
 
 
 
 
     return total
 
-errorCatcherForStatements()
+#errorCatcherForStatements(checkKeyWord = "FRAME")
--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from errorCatcherForStatements import errorCatcherForStatements
 
 def main():
+    choice = -1
 
-    keyword = errorCatcherForStatements()
+    while choice != 0:
+        choice = int(input("Enter 1 to search a keyword, \nEnter 0 to Exit: "))
+        if choice == 1:
+            keyword = input("Enter a keyword to search statements: ")
+            errorCatcherForStatements(keyword)
+        else:
+            exit(0)
 
 main()
